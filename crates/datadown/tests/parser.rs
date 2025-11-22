6ee7ee6f933682dd2e7648403f5cbea4a034b624
jenul-ferdinand <jenul15ferdinand@gmail.com>
2025-11-22 13:17:59 +1100

    refactor(tests): code style

--- crates/datadown/tests/parser.rs
+++ crates/datadown/tests/parser.rs
@@ -51,27 +51,38 @@ fn test_simple_heading1() {
 /// INPUT
 /// # Foo
 /// ## Bar
+/// 
+/// OUTPUT
+/// Document:
+///     Heading:
+///         level: 1
+///         children:
+///             "Foo"
+///     Heading:
+///         level: 2
+///         children:
+///             "Bar"
 #[test]
 fn test_simple_heading1_2() {
-    let md = "# Foo\n## Bar";
+    let md = "# Foo\n\n\n\n\n## Bar";
     let ast = parse_markdown(md);
     println!("{:?}", ast);
 
-    if let Node::Document { children: doc_children } = ast {
-        let h1_node = unwrap_node(&doc_children[0]);
+    if let Node::Document { children: document_children } = ast {
+        let h1_node = unwrap_node(&document_children[0]);
         // Heading 1
-        if let Node::Heading { level: h1_level, children: h1_children } = h1_node {
-            assert_eq!(*h1_level, 1);
-            assert_eq!(unwrap_text(&h1_children[0]), "Foo");
+        if let Node::Heading { level, children } = h1_node {
+            assert_eq!(*level, 1);
+            assert_eq!(unwrap_text(&children[0]), "Foo");
         } else {
             panic!("Not a Heading");
         }
         
-        let h2_node = unwrap_node(&doc_children[1]);
+        let h2_node = unwrap_node(&document_children[1]);
         // Heading 2
-        if let Node::Heading { level: h2_level, children: h2_children } = h2_node {
-            assert_eq!(*h2_level, 2);
-            assert_eq!(unwrap_text(&h2_children[0]), "Bar");
+        if let Node::Heading { level, children } = h2_node {
+            assert_eq!(*level, 2);
+            assert_eq!(unwrap_text(&children[0]), "Bar");
         } else {
             panic!("Not a heading");
         }
